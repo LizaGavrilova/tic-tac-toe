c85884dbf1a197b60cdbfa9dfaa87c39d4ce4ad2
Liza <gliza.gavrilova@yandex.ru>
2022-09-15 20:24:03 +0300

    Create React App

---
+++ src/components/App/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+import './App.scss';
+
+export default class App extends Component {
+  // state = {
+  // };
+
+  render() {
+    return (
+      <h1>Хаюшки</h1>    
+    );
+  }
+}
